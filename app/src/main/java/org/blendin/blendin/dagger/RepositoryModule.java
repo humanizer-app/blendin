db7ec7f46e9bf76c99ef342cc1434a0d05fa8acf
Yurii Andrusiak <yurii.andrusiak@transferwise.com>
2017-06-10 18:47:23 +0300

    Added firebase repo

--- app/src/main/java/org/blendin/blendin/dagger/RepositoryModule.java
+++ app/src/main/java/org/blendin/blendin/dagger/RepositoryModule.java
@@ -1,5 +1,19 @@
 package org.blendin.blendin.dagger;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
 public class RepositoryModule {
 
+    @Singleton
+    @Provides
+    DatabaseReference provideDatabaseReference() {
+        return FirebaseDatabase.getInstance().getReference();
+    }
 }
--- app/src/main/java/org/blendin/blendin/repository/PostsRepository.java
+++ app/src/main/java/org/blendin/blendin/repository/PostsRepository.java
@@ -1,20 +1,44 @@
 package org.blendin.blendin.repository;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.blendin.blendin.models.Post;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.inject.Inject;
 
 public class PostsRepository {
 
-//    FirebaseDatabase database;
-//    = FirebaseDatabase.getInstance();
-//    DatabaseReference myRef = database.getReference("message");
+    private final DatabaseReference database;
+    private final FirebaseAuth auth;
 
     @Inject
-    public PostsRepository() {
+    public PostsRepository(DatabaseReference database, FirebaseAuth auth) {
+        this.database = database;
+        this.auth = auth;
+    }
+
+    public void writeNewPost(String username, String title, String body) {
+        String userId = auth.getCurrentUser().getUid();
+        // Create new post at /user-posts/$userid/$postid and at
+        // /posts/$postid simultaneously
+        String key = database.child("posts").push().getKey();
+        Post post = new Post(userId, username, title, body);
+        Map<String, Object> postValues = post.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/posts/" + key, postValues);
+        childUpdates.put("/user-posts/" + userId + "/" + key, postValues);
+
+        database.updateChildren(childUpdates);
     }
 
     public void fetchPosts() {
-
+        DatabaseReference ref = database.child("posts");
+        ref.push().setValue("Hello, World!");
     }
 }
