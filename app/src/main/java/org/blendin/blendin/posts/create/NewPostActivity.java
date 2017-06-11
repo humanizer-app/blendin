51247349884de39ff5e03bc80e795ae0cf5f8e06
Yurii Andrusiak <yurii.andrusiak@transferwise.com>
2017-06-11 16:08:38 +0300

    Added button to profile screen

--- app/src/main/java/org/blendin/blendin/posts/create/NewPostActivity.java
+++ app/src/main/java/org/blendin/blendin/posts/create/NewPostActivity.java
@@ -3,9 +3,6 @@ package org.blendin.blendin.posts.create;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-
 import org.blendin.blendin.R;
 import org.blendin.blendin.dagger.AppComponent;
 import org.blendin.blendin.dagger.DaggerAppComponent;
@@ -18,13 +15,13 @@ public class NewPostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
 
-        ImageButton sendButton = (ImageButton) findViewById(R.id.sendBtn);
+        View sendButton = findViewById(R.id.sendBtn);
         final AppComponent component = DaggerAppComponent.create();
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 component.postsRepo().addPost(new Post(component.userRepo().getUser(),
-                        "Neeed help", "Desciption of my serious issue", System.currentTimeMillis(),
+                        "Need help", "Description of my serious issue", System.currentTimeMillis(),
                         "Finance"));
                 finish();
             }
